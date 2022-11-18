67dc1a42a8a73825fa34e6e9b466e2915a73a002
MJ Sung <themrsung@gmail.com>
2022-11-18 14:54:02 +0900

    changes

--- scripts/init.js
+++ scripts/init.js
@@ -1,10 +1,8 @@
 $(document).ready(function() { onPageLoad() })
 
 function onPageLoad() {
-    const content = document.getElementById("content")
-
-    content.innerHTML = `<p>test</p>`
-
+    $("#title").load("..pages/templates/title.html")
     $("#content").load("../pages/templates/newsfeed.html")
+    $("#footer").load("../pages/templates/footer.html")
 }
     
