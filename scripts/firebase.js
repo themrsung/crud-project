ac0835470008a30d64dfdb1eada31d2d042cce3c
MJ Sung <themrsung@gmail.com>
2022-11-18 22:08:35 +0900

    dd

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -9,13 +9,13 @@ import { getStorage } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-s
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCYcKPQMIlczBgdij3vkjfx-E5LE8s_Il8",
-  authDomain: "infinity-81338.firebaseapp.com",
-  projectId: "infinity-81338",
-  storageBucket: "infinity-81338.appspot.com",
-  messagingSenderId: "1018635074378",
-  appId: "1:1018635074378:web:900e1364cf941a8cb75fb3",
-  measurementId: "G-X9XBCDXL5R"
+    apiKey: "AIzaSyCYcKPQMIlczBgdij3vkjfx-E5LE8s_Il8",
+    authDomain: "infinity-81338.firebaseapp.com",
+    projectId: "infinity-81338",
+    storageBucket: "infinity-81338.appspot.com",
+    messagingSenderId: "1018635074378",
+    appId: "1:1018635074378:web:900e1364cf941a8cb75fb3",
+    measurementId: "G-X9XBCDXL5R"
 };
 
 // Initialize Firebase
@@ -23,3 +23,26 @@ export const app = initializeApp(firebaseConfig);
 export const dbService = getFirestore(app);
 export const authService = getAuth(app);
 export const storageService = getStorage(app);
+
+
+
+// 게시글 데이터 관리
+// collection(dbService, "posts")
+
+// doc {
+//     title: "제목",
+//     content: "내용",
+//     createdAt: "생성시간",
+//     createdBy: "유저ID",
+// }
+
+// 유저 데이터 관리
+// collections(dbService, "users")
+
+// doc {
+//     userId: "유저ID (고유번호)",
+//     email: "이메일",
+//     name: "이름",
+//     password: "비밀번호",
+//     motd: "상태메시지"
+// }
