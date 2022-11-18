65dee07f189d3cc625bf73d1fa8b62143d991bb8
MJ Sung <themrsung@gmail.com>
2022-11-18 14:51:47 +0900

    asthetics

--- scripts/init.js
+++ scripts/init.js
@@ -1,25 +1,10 @@
-$(document).ready(function() {
+$(document).ready(function() { onPageLoad() })
 
-    const title = document.title
-    const description = document.querySelector("meta[name='description'").content
-
-    var header = document.getElementById("title")
-    header.innerHTML = `
-    <h1>${title}</h1>
-    <p>${description}</p>
-    `
-
-    var footer = document.getElementById("footer")
-    footer.innerHTML = `
-    <h3>Made by INFINITY</h3>
-    <p>Copyright 2022 INFINITY, All Rights Reserved.</p>
-    `
-
+function onPageLoad() {
     const content = document.getElementById("content")
 
     content.innerHTML = `<p>test</p>`
 
     $("#content").load("../pages/templates/newsfeed.html")
-
-})
+}
     
