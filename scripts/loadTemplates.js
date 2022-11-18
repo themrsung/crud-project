f5df6b2eb28ce345695ff76ea80a6fabaf6ec818
MJ Sung <themrsung@gmail.com>
2022-11-18 13:11:11 +0900

    script strip comments

--- scripts/loadTemplates.js
+++ scripts/loadTemplates.js
@@ -1,15 +1,3 @@
-// var host = process.env.HOST || "0.0.0.0"
-// var port = process.env.PORT || 8080
-
-// var corsProxy = require("cors-anywhere")
-// corsProxy.createServer({
-//     originWhiteList: [],
-//     requireHeader: ["origin", "x-requested-with"],
-//     removeHeaders: ["cookie", "cookie2"]
-// }).listen(port, host, function() {
-//     console.log("Running CORS Anywhere on " + host + ":" + port)
-// })
-
 var title = document.title
 var description = document.querySelector("meta[name='description'").content
 
