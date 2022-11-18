8e196f8f5082d5c441dfdfc93e9fd36bc61ac03d
MJ Sung <themrsung@gmail.com>
2022-11-18 14:40:17 +0900

    change

--- scripts/init.js
+++ scripts/init.js
@@ -1,5 +1,25 @@
-const content = document.getElementById("content")
+$(document).ready(function() {
 
-content.innerHTML = `<p>test</p>`
+    const title = document.title
+    const description = document.querySelector("meta[name='description'").content
 
-$("#content").load("../pages/templates/newsfeed.html")
+    var header = document.getElementById("title")
+    header.innerHTML = `
+    <h1>${title}</h1>
+    <p>${description}</p>
+    `
+
+    var footer = document.getElementById("footer")
+    footer.innerHTML = `
+    <h3>Made by INFINITY</h3>
+    <p>Copyright 2022 INFINITY, All Rights Reserved.</p>
+    `
+
+    const content = document.getElementById("content")
+
+    content.innerHTML = `<p>test</p>`
+
+    $("#content").load("../pages/templates/newsfeed.html")
+
+})
+    
