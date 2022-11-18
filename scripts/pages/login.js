04b93af345033bba84354663e656ef0825f1dc08
freesian12 <zzte123@naver.com>
2022-11-18 21:49:16 +0900

    로그인 페이지 관련

---
+++ scripts/pages/login.js
@@ -0,0 +1,50 @@
+function logIn()
+{
+    const $email = document.getElementById("email");
+    const $password = document.getElementById("password");
+    
+    // 이메일 잘 기입 되었는지
+    if ($email.value === "")
+    {
+        alert("이메일을 입력해주세요");
+        //$email.focus();
+        return
+    
+    }
+    // 비밀번호 잘 쳤는지
+    else if (password.value === "")
+    {
+        alert("비밀번호를 입력해주세요");
+        //$password.focus();
+        return
+    }
+    //자동로그인, 아이디저장 유무
+    
+
+    // 로그인 실패 할때
+
+
+    // 로그인 됐을때
+    // -> 자동로그인, 아이디저장 유무에 따른 쿠키 설정 ()
+}
+
+
+function googleLogIn()
+{
+    
+
+    // 로그인 실패 할때
+    // 로그인 됐을때
+    // -> 자동로그인, 아이디저장 유무에 따른 쿠키 설정 ()
+}
+
+function gitLogIn()
+{
+    
+    
+
+    // 로그인 실패 할때
+    // 로그인 됐을때
+    // -> 자동로그인, 아이디저장 유무에 따른 쿠키 설정 ()
+}
+
