d133e8d9904e424ecc90f90ef6ff121a37ac6b29
sinra <117060851+rhsok@users.noreply.github.com>
2022-11-21 14:52:23 +0900

    회원가입 기능

--- scripts/pages/register.js
+++ scripts/pages/register.js
@@ -1,7 +1,7 @@
 import { authService } from "../firebase.js";
 import {
     createUserWithEmailAndPassword, // 회원가입 메서드
-    signInWithEmailAndPassword, // 로그인 메서드
+ //   signInWithEmailAndPassword, // 로그인 메서드
 //   GoogleAuthProvider, // 구글 로그인 메서드
 //   signInWithPopup, // 로그인 방법별 메서드 (구글,깃 로그인 시 필요 메소드)
 //   GithubAuthProvider, // 깃 로그인 메서드
@@ -12,3 +12,30 @@ import { loadNewsfeed } from "../script.js"
 window.registerComplete = function() {
     loadNewsfeed()
 }
+
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js ";
+    document.getElementById('signUpButton').addEventListener('click', (event) => {
+    event.preventDefault()
+    console.log(1)
+    const email = document.getElementById('signUpEmail').value
+    const password = document.getElementById('signUpPassword').value
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        console.log(userCredential)
+        // Signed in 
+        const user = userCredential.user;
+        // ...
+    })
+    .catch((error) => {
+        console.log('error')
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+    });
+    })
+
+
+// window.regLoadLandingPage = function() {
+//   window.location.replace("../pages/landing.html")
+// }
