440fff9002b7e38d861ab796d75db0767b24f74c
MJ Sung <themrsung@gmail.com>
2022-11-21 16:27:24 +0900

    asd

--- scripts/pages/register.js
+++ scripts/pages/register.js
@@ -3,12 +3,34 @@ import { loadNewsfeed } from "../script.js"
 
 window.registerComplete = function() {
     loadNewsfeed()
+    onRegisterButtonClicked()
 }
 
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js ";
-    document.getElementById('signUpButton').addEventListener('click', (event) => {
-    event.preventDefault()
-    console.log(1)
+
+
+// document.getElementById('signUpButton').addEventListener('click', (event) => {
+//     event.preventDefault()
+//     console.log(1)
+//     const email = document.getElementById('signUpEmail').value
+//     const password = document.getElementById('signUpPassword').value
+//     const auth = getAuth();
+//     createUserWithEmailAndPassword(auth, email, password)
+//     .then((userCredential) => {
+//         console.log(userCredential)
+//         // Signed in 
+//         const user = userCredential.user;
+//         // ...
+//     })
+//     .catch((error) => {
+//         console.log('error')
+//         const errorCode = error.code;
+//         const errorMessage = error.message;
+//         // ..
+//     });
+// })
+
+function onRegisterButtonClicked() {
     const email = document.getElementById('signUpEmail').value
     const password = document.getElementById('signUpPassword').value
     const auth = getAuth();
@@ -25,7 +47,7 @@ import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com
         const errorMessage = error.message;
         // ..
     });
-    })
+}
 
 
 // window.regLoadLandingPage = function() {
