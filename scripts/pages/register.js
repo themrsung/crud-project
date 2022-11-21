af860dc29fc4f4d623ab866cfd42ea5b50c4725e
MJ Sung <themrsung@gmail.com>
2022-11-21 16:44:21 +0900

    제발

--- scripts/pages/register.js
+++ scripts/pages/register.js
@@ -7,9 +7,28 @@ window.registerComplete = function() {
 }
 
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js ";
-    document.getElementById('signUpButton').addEventListener('click', (event) => {
-    event.preventDefault()
-    console.log(1)
+//     document.getElementById('signUpButton').addEventListener('click', (event) => {
+//     event.preventDefault()
+//     console.log(1)
+//     const email = document.getElementById('signUpEmail').value
+//     const password = document.getElementById('signUpPassword').value
+//     const auth = getAuth();
+//     createUserWithEmailAndPassword(auth, email, password)
+//     .then((userCredential) => {
+//         console.log(userCredential)
+//         // Signed in 
+//         const user = userCredential.user;
+//         // ...
+//     })
+//     .catch((error) => {
+//         console.log('error')
+//         const errorCode = error.code;
+//         const errorMessage = error.message;
+//         // ..
+//     });
+// }
+
+function onRegisterButtonClicked() {
     const email = document.getElementById('signUpEmail').value
     const password = document.getElementById('signUpPassword').value
     const auth = getAuth();
