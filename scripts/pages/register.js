608f88f7d5ce059e989880061cd82cd896b281e5
MJ Sung <themrsung@gmail.com>
2022-11-21 16:23:55 +0900

    dd

--- scripts/pages/register.js
+++ scripts/pages/register.js
@@ -1,12 +1,4 @@
 import { authService } from "../firebase.js";
-import {
-    createUserWithEmailAndPassword, // 회원가입 메서드
- //   signInWithEmailAndPassword, // 로그인 메서드
-//   GoogleAuthProvider, // 구글 로그인 메서드
-//   signInWithPopup, // 로그인 방법별 메서드 (구글,깃 로그인 시 필요 메소드)
-//   GithubAuthProvider, // 깃 로그인 메서드
-//   signOut, // 로그아웃 메서드
-} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
 import { loadNewsfeed } from "../script.js"
 
 window.registerComplete = function() {
