59e30566d551186ce467a3fd284da6a2eb729075
sinra <117060851+rhsok@users.noreply.github.com>
2022-11-22 19:12:41 +0900

    찾기 실패시 메세지변경

--- scripts/pages/lostaccount.js
+++ scripts/pages/lostaccount.js
@@ -13,14 +13,13 @@ function resetBtn(){
     const auth = getAuth();
     sendPasswordResetEmail(auth, email.value)
     .then(() => {
-        console.log("Reset Password!")
         alert('메일로 Password 메세지를 보냈습니다.\n(이메일이 안 왔다면 spam함을 확인해보세요!)')
         $("#content").load("../pages/templates/login.html")
         // Password reset email sent!
         // ..
     })
     .catch((error) => {
-        alert('실패했습니다 ')
+        alert('가입되지 않은 이메일입니다.')
         $("#content").load("../pages/templates/lostaccount.html")
         const errorCode = error.code;
         const errorMessage = error.message;
