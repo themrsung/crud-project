00393a75e121f254b7d1794009ab8d88ad2313d9
freesian12 <zzte123@naver.com>
2022-11-24 09:23:33 +0900

    qwe

--- scripts/pages/writepost.js
+++ scripts/pages/writepost.js
@@ -40,3 +40,14 @@ window.writePost = function() {
 }
 
 // document.querySelector("#write-post-button").addEventListener("click", writePost)
+window.fileControl = (event) => {
+    const theFile = event.target.files[0]; // file 객체
+    const reader = new FileReader();
+    reader.readAsDataURL(theFile); // file 객체를 브라우저가 읽을 수 있는 data URL로 읽음.
+    reader.onloadend = (finishedEvent) => {
+      // 파일리더가 파일객체를 data URL로 변환 작업을 끝났을 때
+      const imgDataUrl = finishedEvent.currentTarget.result;
+      localStorage.setItem("imgDataUrl", imgDataUrl);
+      document.getElementById("profileView").src = imgDataUrl;
+    };
+  };
